5fcb2456c8d7e6e8b938ee5686733c77904aa139
James Ford <psyked84@gmail.com>
2018-06-13 12:45:14 +0100

    Expand colours to other formats

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
+// JS
 import React from 'react'
 import { Card } from 'semantic-ui-react'
+import extractor from 'css-color-extractor'
+import parse from 'parse-color'
 
+// CSS
 import "semantic-ui-css/semantic.css";
-
 import styles from './styles.module.css'
+
+// Raw data
 import exampleData from 'raw!../master.source'
 
-
-var extractor = require('css-color-extractor');
 
 var options = {
   // withoutGrey: false, // set to true to remove rules that only have grey colors
@@ -18,17 +21,49 @@ var options = {
 // extract from a full stylesheet
 const extractedColours = extractor.fromCss(exampleData);
 
+// expand the input colours into their other syntax equivalents
+const expanded = extractedColours.map(parse)
+console.log(expanded)
+
 const IndexPage = () => (
   <div>
-    <h2>Extracted Colours <small>({extractedColours.length} total)</small></h2>
+    <h1>Summary</h1>
+    <Card>
+      <Card.Content>
+        <Card.Header>Extracted data</Card.Header>
+        <Card.Description>
+          <table><tbody>
+            <tr><td>Total Color Declarations:</td><td>{extractedColours.length}</td></tr>
+          </tbody>
+          </table>
+        </Card.Description>
+      </Card.Content>
+    </Card>
+    <h2>Extracted Colours</h2>
     <Card.Group className={styles.palettecontainer}>
       {
-        extractedColours.map((colour) => {
+        expanded.map((colour) => {
           return (
-            <Card key={colour}>
+            <Card key={colour.hex}>
               <Card.Content>
-                <Card.Header>{colour}</Card.Header>
-                <div className={`ui mini right floated ${styles.palette}`} style={{ backgroundColor: colour }}></div>
+                <div className={`ui mini right floated ${styles.palette}`} style={{ backgroundColor: colour.hex }}></div>
+                <Card.Header>{colour.hex}</Card.Header>
+                <Card.Meta>{colour.keyword}</Card.Meta>
+                <Card.Description>
+                  <table><tbody>
+                    <tr><td>rgb</td><td>{String(colour.rgb)}</td></tr>
+                    <tr><td>hsl</td><td>{String(colour.hsl)}</td></tr>
+                    <tr><td>hsv</td><td>{String(colour.hsv)}</td></tr>
+                    <tr><td>cmyk</td><td>{String(colour.cmyk)}</td></tr>
+                    <tr><td>keyword</td><td>{String(colour.keyword)}</td></tr>
+                    <tr><td>hex</td><td>{String(colour.hex)}</td></tr>
+                    <tr><td>rgba</td><td>{String(colour.rgba)}</td></tr>
+                    <tr><td>hsla</td><td>{String(colour.hsla)}</td></tr>
+                    <tr><td>hsva</td><td>{String(colour.hsva)}</td></tr>
+                    <tr><td>cmyka</td><td>{String(colour.cmyka)}</td></tr>
+                  </tbody>
+                  </table>
+                </Card.Description>
               </Card.Content>
             </Card>
           )
