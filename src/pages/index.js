7f51038ff82117e5457ec2a62c896541c499900e
James Ford <psyked84@gmail.com>
2018-06-13 13:59:41 +0100

    Add neater display for raw values and parsed values, and enable hotlinks to colours

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,46 +22,101 @@ var options = {
 const extractedColours = extractor.fromCss(exampleData);
 
 // expand the input colours into their other syntax equivalents
-const expanded = extractedColours.map(parse)
-console.log(expanded)
+const expanded = extractedColours
+  .map(declaration => {
+    return {
+      ...parse(declaration),
+      raw: declaration
+    }
+  }) // replace with the full details
+  .filter(({ hex }) => !!hex) // remove any that haven't parsed correctly
+
+function removeDuplicates(myArr, prop) {
+  return myArr.filter((obj, pos, arr) => {
+    return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+  });
+}
+
+const deduplicated = removeDuplicates(expanded, 'hex');
 
 const IndexPage = () => (
   <div>
     <h1>Summary</h1>
-    <Card>
+    <p>The <a href="#source-data">master CSS file</a> from OpenLibrary has been embedded into this page and processed.</p>
+    <Card fluid>
       <Card.Content>
         <Card.Header>Extracted data</Card.Header>
+      </Card.Content>
+      <Card.Content extra>
         <Card.Description>
-          <table><tbody>
-            <tr><td>Total Color Declarations:</td><td>{extractedColours.length}</td></tr>
-          </tbody>
+          <table>
+            <tbody>
+              <tr>
+                <td>Total Color Declarations:</td>
+                <td>{extractedColours.length}</td>
+                {/* <td></td> */}
+              </tr>
+              <tr>
+                <td>Unique Colors:</td>
+                <td>{deduplicated.length}</td>
+                {/* <td></td> */}
+              </tr>
+              <tr>
+                <td>CSS Spec Keyword Colors:</td>
+                <td>{deduplicated.filter(({ keyword }) => !!keyword).length}</td>
+                {/* <td>{
+                  deduplicated.filter(({ keyword }) => !!keyword).map(({ hex, keyword }) => {
+                    return <span><a href={`${hex}`}>{keyword}</a>, </span>
+                  })
+                }</td> */}
+              </tr>
+            </tbody>
           </table>
         </Card.Description>
       </Card.Content>
     </Card>
     <h2>Extracted Colours</h2>
-    <Card.Group className={styles.palettecontainer}>
+    <p>Using <a href="https://github.com/rsanchez/css-color-extractor">css-color-extractor</a> to extract color declarations from CSS source and <a href="https://github.com/substack/parse-color">parse-color</a> to translate declarations into alternative formats.</p>
+    <Card.Group className={styles.palettecontainer} itemsPerRow={3}>
       {
         expanded.map((colour) => {
           return (
             <Card key={colour.hex}>
               <Card.Content>
                 <div className={`ui mini right floated ${styles.palette}`} style={{ backgroundColor: colour.hex }}></div>
-                <Card.Header>{colour.hex}</Card.Header>
+                <Card.Header><a name={colour.hex.replace('#', '')}></a>{colour.hex}</Card.Header>
                 <Card.Meta>{colour.keyword}</Card.Meta>
+              </Card.Content>
+              <Card.Content extra>
                 <Card.Description>
-                  <table><tbody>
-                    <tr><td>rgb</td><td>{String(colour.rgb)}</td></tr>
-                    <tr><td>hsl</td><td>{String(colour.hsl)}</td></tr>
-                    <tr><td>hsv</td><td>{String(colour.hsv)}</td></tr>
-                    <tr><td>cmyk</td><td>{String(colour.cmyk)}</td></tr>
-                    <tr><td>keyword</td><td>{String(colour.keyword)}</td></tr>
-                    <tr><td>hex</td><td>{String(colour.hex)}</td></tr>
-                    <tr><td>rgba</td><td>{String(colour.rgba)}</td></tr>
-                    <tr><td>hsla</td><td>{String(colour.hsla)}</td></tr>
-                    <tr><td>hsva</td><td>{String(colour.hsva)}</td></tr>
-                    <tr><td>cmyka</td><td>{String(colour.cmyka)}</td></tr>
-                  </tbody>
+                  <table className="ui celled table">
+                    <thead>
+                      <tr>
+                        <th colSpan="2">Existing usage</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr><td><strong>raw</strong></td><td>{String(colour.raw)}</td></tr>
+                    </tbody>
+                  </table>
+                  <table className="ui celled table">
+                    <thead>
+                      <tr>
+                        <th colSpan="2">Parsed color values</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr><td><strong>hex</strong></td><td>{String(colour.hex)}</td></tr>
+                      <tr><td><strong>rgb</strong></td><td>{String(colour.rgb)}</td></tr>
+                      <tr><td><strong>rgba</strong></td><td>{String(colour.rgba)}</td></tr>
+                      <tr><td><strong>hsl</strong></td><td>{String(colour.hsl)}</td></tr>
+                      <tr><td><strong>hsla</strong></td><td>{String(colour.hsla)}</td></tr>
+                      <tr><td><strong>hsv</strong></td><td>{String(colour.hsv)}</td></tr>
+                      <tr><td><strong>hsva</strong></td><td>{String(colour.hsva)}</td></tr>
+                      <tr><td><strong>cmyk</strong></td><td>{String(colour.cmyk)}</td></tr>
+                      <tr><td><strong>cmyka</strong></td><td>{String(colour.cmyka)}</td></tr>
+                      <tr className={!colour.keyword ? 'disabled' : ''}><td><strong>keyword</strong></td><td className={!colour.keyword ? styles.disabled : ''}>{!!colour.keyword ? String(colour.keyword) : 'no match'}</td></tr>
+                    </tbody>
                   </table>
                 </Card.Description>
               </Card.Content>
@@ -70,7 +125,7 @@ const IndexPage = () => (
         })
       }
     </Card.Group>
-    <h2>Source data</h2>
+    <h2 id="source-data">Source data</h2>
     <pre><code>{exampleData}</code></pre>
   </div >
 )
